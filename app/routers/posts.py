f458135ac207bc1f79c03f1dce7bfb87ff9a2d67
exxiidae <selevuv@mail.ru>
2025-12-25 04:23:13 +0300

    Update posts.py

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from .. import models, schemas, database
+from app import models, schemas
+from app.database import get_db
 
 router = APIRouter()
 
 @router.post("/posts", response_model=schemas.PostResponse)
-def create_post(post: schemas.PostCreate, db: Session = Depends(database.get_db)):
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     db_post = models.Post(**post.model_dump())
     db.add(db_post)
     db.commit()
@@ -13,12 +14,12 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(database.get_db)
     return db_post
 
 @router.get("/posts", response_model=list[schemas.PostResponse])
-def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(database.get_db)):
+def read_posts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     posts = db.query(models.Post).offset(skip).limit(limit).all()
     return posts
 
 @router.get("/posts/{post_id}", response_model=schemas.PostResponse)
-def read_post(post_id: int, db: Session = Depends(database.get_db)):
+def read_post(post_id: int, db: Session = Depends(get_db)):
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
